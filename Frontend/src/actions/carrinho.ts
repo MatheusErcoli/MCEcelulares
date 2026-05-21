bb25c4df7de6fc3eee50edf4faee7e85a6043fe9
MatheusErcoli <matheusercoli@gmail.com>
2026-05-21 17:26:01 -0300

    Feat: Segurança no Nginx (cabeçalhos HTTP)

--- Frontend/src/actions/carrinho.ts
+++ Frontend/src/actions/carrinho.ts
@@ -1,6 +1,6 @@
 import { fetchWithAuth } from "../lib/fetchWithAuth";
 
-const API_URL = 'http://localhost:3000';
+const API_URL = '/api';
 
 export async function createCarrinhoAPI(token: string) {
   try {
