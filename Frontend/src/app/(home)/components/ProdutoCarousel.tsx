b933a45c56451e6f5c2bdf59dedcf77793d6ab8b
Raf0627 <rafaelfrossard076@gmail.com>
2026-06-04 18:52:57 -0300

    feat: front responsivo no mobile

--- Frontend/src/app/(home)/components/ProdutoCarousel.tsx
+++ Frontend/src/app/(home)/components/ProdutoCarousel.tsx
@@ -22,15 +22,39 @@ export const ProdutoCarousel = ({ destaque }: ProdutoCarouselProps) => {
     }, [execute, destaque]);
 
     const settings = {
-        dots: true,
-        infinite: true,
+        dots: true, // Mantém os dots ativos por padrão (para 4, 3 e 2 itens)
+        infinite: produtos.length > 1,
         speed: 500,
         slidesToShow: 4,
         slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1280,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 640, 
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: false
+                }
+            }
+        ]
     };
 
     return (
-        <div className="w-full max-w-7xl m-auto px-12 pb-15">
+        <div className="w-full max-w-7xl m-auto px-6 md:px-12 pb-15">
             {loading && (
                 <p className="text-center font-medium text-gray-400 animate-pulse">
                     Carregando produtos...
@@ -46,16 +70,19 @@ export const ProdutoCarousel = ({ destaque }: ProdutoCarouselProps) => {
             {!loading && !error && produtos.length === 0 && (
                 <div className="flex flex-col items-center justify-center py-20 gap-3 text-gray-400">
                     <Icon name="faMobileScreen" />
-                    <p className="text-sm font-medium">Nenhuma produto encontrado.</p>
+                    <p className="text-sm font-medium">Nenhum produto encontrado.</p>
                 </div>
             )}
-            <div className="m-0 p-0">
-                <Slider {...settings}>
-                    {produtos.map((p) => (
-                        <ProdutoCard key={p.id_produto} produto={p} />
-                    ))}
-                </Slider>
-            </div>
+
+            {!loading && !error && produtos.length > 0 && (
+                <div className="m-0 p-0">
+                    <Slider {...settings}>
+                        {produtos.map((p) => (
+                            <ProdutoCard key={p.id_produto} produto={p} />
+                        ))}
+                    </Slider>
+                </div>
+            )}
         </div>
     );
 };
